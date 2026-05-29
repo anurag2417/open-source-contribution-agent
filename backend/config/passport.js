894a53e94f5be8be673e98459dde3c5e8e216618
Anurag Kumar <anuragakn18@gmail.com>
2026-05-29 16:43:26 +0530

    Prepare frontend for Vercel deployment

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -9,7 +9,7 @@ passport.use(
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       callbackURL:
-        "http://localhost:5000/api/auth/github/callback",
+        "http://process.env.NEXT_PUBLIC_API_URL/api/auth/github/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
